670f1b8b10d1d8ac86ff3113b0db5c2cb6c724f0
Jenique Knoesen <Jenique82@gmail.com>
2022-02-14 12:52:13 +0200

    adding redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
+import{ setSearchField} from '../actions';
 
+const mapStateToProps = state => {
+  return{
+    searchField : state.searchField
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return { 
+  onSearchChange : (event) => dispatch(setSearchField(event.target.value))
+}
+}
 class App extends Component {
   constructor() {
     super()
     this.state = {
       robots: [],
-      searchfield: ''
+     
     }
   }
 
@@ -19,14 +32,12 @@ class App extends Component {
       .then(users => {this.setState({ robots: users})});
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-  }
-
+ 
   render() {
-    const { robots, searchfield } = this.state;
+    const { robots } = this.state;
+    const {searchField, onSearchChange} =this.props;
     const filteredRobots = robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
     return !robots.length ?
       <h1>Loading</h1> :
@@ -42,4 +53,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import {createLogger} from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import { searchRobots } from './reducers';
 import 'tachyons'; 
 
+const logger = createLogger();
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+const store = createStore (searchRobots, applyMiddleware(thunkMiddleware, logger))
+
+ReactDOM.render(<Provider store={store}> <App  /></Provider>, document.getElementById('root'));
+registerServiceWorker();
